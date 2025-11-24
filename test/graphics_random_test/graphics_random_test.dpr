0e281f47e7bed3af7e0b076f0c6ee4d52257a94d
Michalis Kamburelis <michalis.kambi@gmail.com>
2025-11-24 15:29:37 +0100

    Fix to use castle_engine_base

--- test/graphics_random_test/graphics_random_test.dpr
+++ test/graphics_random_test/graphics_random_test.dpr
@@ -7,7 +7,7 @@ uses
   cthreads,
   {$ENDIF}{$ENDIF}
   Interfaces, // this includes the LCL widgetset
-  Forms, GraphicsTestUnit, castle_base
+  Forms, GraphicsTestUnit
   { you can add units after this };
 
 {$R *.res}
